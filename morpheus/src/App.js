44d1c1066b804f5ac6e2b2c65b66df3819f36314
niuyazhe <niuyazhe@sensetime.com>
2024-01-05 15:22:35 +0800

    feature(nyz): add format data function

--- morpheus/src/App.js
+++ morpheus/src/App.js
@@ -1,39 +1,7 @@
 import React, { Component } from 'react';
 import Poster from 'components/poster-template';
 import Entry from 'components/entry-template';
-
-
-function formatData(rawData) {
-    const data = rawData.result
-    const statusCode = rawData.statusCode
-    if (statusCode !== 200) {
-        return {
-            totalOperation: 0,
-            commitOperation: 0,
-            PRIssueOperation: 0,
-            latestMoment: '00:00',
-            favoriteRepo: '',
-            label: '',
-            languageRanking: [],
-            imgUrl: '',
-            repoTopic: 0,
-        }
-    } else {
-        const tmp = data.later_time.split('-')
-        const latestMoment = `${tmp[tmp.length - 3]}:${tmp[tmp.length - 2]}`
-        return {
-            totalOperation: data.total_operate,
-            commitOperation: data.commit_operate,
-            PRIssueOperation: data.issue_and_pullrequest,
-            latestMoment: latestMoment,
-            favoriteRepo: data.favorite_repo,
-            label: data.label.split('\"')[3],
-            languageRanking: Object.entries(data.language_ranking).map(item => {return { name: item[0], value: item[1] }}),
-            imgUrl: data.img_url,
-            repoTopic: data.topic_total,
-        }
-    }
-}
+import { formatData } from 'components/format-data';
 
 
 class App extends Component {
@@ -49,8 +17,7 @@ class App extends Component {
     if (username === '') {
         return
     }
-    //const url = process.env.SERVER_URL || 'http://localhost:3000';
-    const url = "http://8.218.102.192:8006"
+    const url = process.env.REACT_APP_SERVER_URL || 'http://localhost:3000';
     
     fetch(
         `${url}/get_data`,
---
+++ morpheus/src/components/format-data.js
@@ -0,0 +1,31 @@
+export function formatData(rawData) {
+    const data = rawData.result
+    const statusCode = rawData.statusCode
+    if (statusCode !== 200) {
+        return {
+            totalOperation: 0,
+            commitOperation: 0,
+            PRIssueOperation: 0,
+            latestMoment: '00:00',
+            favoriteRepo: '',
+            label: '',
+            languageRanking: [],
+            imgUrl: '',
+            repoTopic: 0,
+        }
+    } else {
+        const tmp = data.later_time.split('-')
+        const latestMoment = `${tmp[tmp.length - 3]}:${tmp[tmp.length - 2]}`
+        return {
+            totalOperation: data.total_operate,
+            commitOperation: data.commit_operate,
+            PRIssueOperation: data.issue_and_pullrequest,
+            latestMoment: latestMoment,
+            favoriteRepo: data.favorite_repo,
+            label: data.label.split('\"')[3],
+            languageRanking: Object.entries(data.language_ranking).map(item => {return { name: item[0], value: item[1] }}),
+            imgUrl: data.img_url,
+            repoTopic: data.topic_total,
+        }
+    }
+}
